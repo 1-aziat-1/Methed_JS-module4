5a72f9388f9dfe05145a0df6177fb27e6713459c
1-aziat-1 <as.ac2727@mail.ru>
2023-02-02 03:45:36 +0300

    lesson2 ready

--- js/index.js
+++ js/index.js
@@ -1,10 +1,24 @@
 "use strict";
 
-const product = 'banana';
-const number = 20;
-const category = 'fruit';
-const price = 50;
+// const product = 'banana';
+// const number = 20;
+// const category = 'fruit';
+// const price = 50;
 
 
-console.log(product);
-console.log(number * price);
+// console.log(product);
+// console.log(number * price);
+
+//урок 2!!!!!!!!!!!!!!!!
+
+const product = prompt("Наименование товара?");
+const number = +prompt("Количество товара?");
+const category = prompt("Категория товара?");
+const price = +prompt("Цена товара?");
+
+console.log(typeof product);
+console.log(typeof number);
+console.log(typeof category);
+console.log(typeof price);
+
+console.log(`На складе ${number} единицы товара ${product} на сумму ${number * price} деревянных`);
---
+++ js/task1.js
@@ -0,0 +1,4 @@
+const myWeight = 70;
+const speedLight = 3e8;
+const e = myWeight * speedLight**2;
+console.log(`моя энергия - ` +  e);

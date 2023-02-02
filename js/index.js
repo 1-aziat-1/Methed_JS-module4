72ddb2458716c3a171fb91e65dc0993ee8827c9a
1-aziat-1 <as.ac2727@mail.ru>
2023-02-03 02:12:55 +0300

    lesson3 ready easy and hard

--- js/index.js
+++ js/index.js
@@ -1,24 +1,20 @@
 "use strict";
 
-// const product = 'banana';
-// const number = 20;
-// const category = 'fruit';
-// const price = 50;
-
-
-// console.log(product);
-// console.log(number * price);
-
-//урок 2!!!!!!!!!!!!!!!!
-
 const product = prompt("Наименование товара?");
 const number = +prompt("Количество товара?");
 const category = prompt("Категория товара?");
 const price = +prompt("Цена товара?");
 
-console.log(typeof product);
-console.log(typeof number);
-console.log(typeof category);
-console.log(typeof price);
 
 console.log(`На складе ${number} единицы товара ${product} на сумму ${number * price} деревянных`);
+
+let num = prompt('Введите число', '0');
+
+if (num >= 0) {
+  console.log(typeof num);
+  alert("Всё окей!");
+} else {
+  alert("Вы ввели некорректные данные");
+}
+
+
--- js/task1.js
+++ js/task1.js
@@ -1,4 +1,46 @@
-const myWeight = 70;
-const speedLight = 3e8;
-const e = myWeight * speedLight**2;
-console.log(`моя энергия - ` +  e);
+// const myWeight = 70;
+// const speedLight = 3e8;
+// const e = myWeight * speedLight**2;
+// console.log(`моя энергия - ` +  e);
+
+// урок 3 задание 1
+
+{
+  const rain = Math.round(Math.random());
+
+if (rain === 1) {
+console.log('Пошёл дождь. Возьмите зонт!');
+} else {
+console.log('Дождя нет!');
+}
+}
+
+
+//урок 3 задание 2 
+
+{
+const math = + prompt('Введите колличество баллов по математике');
+const russ = + prompt('Введите колличество баллов по русскому языку');
+const inf = + prompt('Введите колличество баллов по информатике');
+const total = 265;
+
+if (math + russ + inf === total) {
+    alert('Поздравляю, вы поступили на бюджет!');
+  } else {
+  alert('Вы не поступили');
+}
+}
+
+//урок 3 задание 3
+
+{
+const many = +prompt('Владимир сколько денег вы хотите снять?');
+
+const minMoney = 100;
+
+if (many % minMoney === 0) {
+  alert('Успешно');
+} else {
+  alert('Ошибка');
+}
+}
